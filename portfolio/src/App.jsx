8ae66ea965d0fb17ee0b4f9e6318dcb32b1a5b62
Joseph Lau <lau.joseph.ca@gmail.com>
2025-04-19 00:53:59 +0800

    setup

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,10 +1,8 @@
 import Hero from "./components/Hero";
-import NavBar from "./components/Navbar";
 
 export default function App() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
-      <NavBar />
       <Hero />
     </main>
   );
