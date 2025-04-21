8144f246db0f9e85d44c5ea4721a5319e3fae81c
Joseph Lau <lau.joseph.ca@gmail.com>
2025-04-22 00:04:38 +0800

    Update Navbar.jsx

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -3,10 +3,25 @@ import React, { useRef } from 'react'
 export default function Navbar() {
     const navContainerRef = useRef(null);
 
-  return (
-    <div>
-        
-    </div>
-  )
-}
+    const navItems = ['Home', 'About Me', 'Projects', 'Resume', 'Contact'];
+
+    return (
+        <div ref={navContainerRef} className="fixed inset-x-0 top-4 z-50 h-16 border-none 
+        transition-all duration-700 sm:inset-x-6">
+            <header className="absolute top-1/2 w-full -translate-y-1/2">
+                <nav className="flex size-full items-center justify-end p-4">
+                    <div className="flex h-full items-center">
+                        <div className="hidden md:block">
+                            {navItems.map((item) => (
+                                <a className="nav-hover-btn">
+                                    {item}
+                                </a>
+                            ))}
+                        </div>
+                    </div>
+                </nav>
+            </header>
+        </div>
+    )
+    }
 
