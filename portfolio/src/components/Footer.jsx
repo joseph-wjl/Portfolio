cfb54a6efea4f222d91f49879608969ec0879ada
Joseph Lau <lau.joseph.ca@gmail.com>
2025-04-22 16:30:10 +0800

    edit

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
 
 export default function Footer() {
     return (
-        <div>footer</div>
+        <div>footer
+            <FaLinkedin />
+            <FaGithub />
+        </div>
     )
 }
