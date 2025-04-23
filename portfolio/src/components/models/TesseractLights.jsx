ea82821d2e896e5520a6fdaa836d2b12769fd050
Joseph Lau <lau.joseph.ca@gmail.com>
2025-04-24 02:44:39 +0800

    Update TesseractLights.jsx

--- portfolio/src/components/models/TesseractLights.jsx
+++ portfolio/src/components/models/TesseractLights.jsx
@@ -4,8 +4,8 @@ import * as THREE from 'three'
 export default function TesseractLights() {
   return (
     <>
-        <ambientLight intensity={0.2} color="#1a1a40" />
-        <directionalLight position={[5, 5, 5]} intensity={7} color="#ffffff" />
+        <ambientLight intensity={15} color="#1a1a40" />
+        <directionalLight position={[5, 5, 5]} intensity={10} color="#d93716 " />
 
         {/* <spotLight 
         position = { [2, 5, 6] }
@@ -24,22 +24,22 @@ export default function TesseractLights() {
         /> */}
 
         <primitive
-        object = { new THREE.RectAreaLight('#A259FF', 8, 3, 2) }
+        object = { new THREE.RectAreaLight('#f37731', 8, 3, 2) }
         position = { [1, 3, 4] }
-        intensity = {15}
+        intensity = {10}
         rotation = {[-Math.PI/4, Math.PI/4, 0]}
         />
 
         <pointLight 
         position = { [0, 1, 0] }
         intensity = {10}
-        color = "#7209b7"
+        color = "#d93716"
         />
 
         <pointLight 
         position = { [1, 2, -2] }
         intensity = {10}
-        color = "#0D00A4"
+        color = "#d93716"
         />
         
     </>
