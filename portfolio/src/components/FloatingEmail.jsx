eb65573b51a5f3c607303c764c05ff458ea74a5b
Joseph Lau <lau.joseph.ca@gmail.com>
2025-04-24 19:25:42 +0800

    Update FloatingEmail.jsx

--- portfolio/src/components/FloatingEmail.jsx
+++ portfolio/src/components/FloatingEmail.jsx
@@ -4,8 +4,9 @@ export default function FloatingEmail() {
   return (
     <a
       href="mailto:jlau.dev@gmail.com"
-      className="fixed right-0 top-1/2 transform -translate-y-1/2 rotate-90 
-      bg-black text-white font-bold py-2 px-4 rounded shadow-lg hover:bg-white hover:text-black transition"
+      className="fixed right-0 top-1/2 transform -translate-y-1/2 translate-x-14 rotate-90 
+      bg-black text-white text-sm font-bold py-1 px-2 rounded shadow-lg 
+      hover:bg-white hover:text-black transition"
     >
       jlau.dev@gmail.com
     </a>
