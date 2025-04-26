f9ea6120d7160ff5a5f4de906b4e8b32b59fca88
Joseph Lau <lau.joseph.ca@gmail.com>
2025-04-26 16:35:48 +0800

    edit

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -5,24 +5,27 @@ export default function About() {
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden flex items-center justify-center bg-yellow-300">
        
-       <div className="about-text">
-            <p>I am an aspiring web developer with a passion for crafting 
+       <div className="about-text flex flex-col h-dvh justify-center font-robert-regular">
+            <p className="py-10 pl-10">
+                I am an aspiring web developer with a passion for crafting 
                 websites that are as beautiful as they are functional. As a self-taught developer, 
                 I’m on a journey of constant learning, diving into new technologies to bring my creative 
                 visions to life. My artistic roots drive me to prioritize design and aesthetics, blending 
                 them seamlessly with solid functionality to create digital experiences that stand out.</p>
-            <p>I believe that great web development is about more than just code—it’s immerse telling a story 
+            <p className="py-10 pl-10">
+                I believe that great web development is about more than just code—it’s immerse telling a story 
                 through thoughtful design and intuitive user experiences. Whether it’s a sleek layout, a 
                 responsive interface, or a perfect balance of form and function, I’m dedicated to making 
                 every project I tackle both visually captivating and technically sound.</p>
-            <p>When I’m not behind the screen, I’m usually out in the world that inspires me—hiking through 
+            <p className="py-10 pl-10">
+                When I’m not behind the screen, I’m usually out in the world that inspires me—hiking through 
                 rugged trails, camping under a starry sky, or soaking up the sun at the beach. Photography is 
                 my way of capturing those moments, while fitness keeps me energized and pushing my limits. These 
                 passions fuel my creativity and shape my approach to building web experiences that feel alive, 
                 dynamic, and connected to the user.</p>
         </div> 
 
-        <Tesseract />
+        <Tesseract className="h-dvh"/>
 
     </div>
   )
