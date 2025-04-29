a96a710f3005404a065c8222b4b47d8c4bc395d3
Joseph Lau <lau.joseph.ca@gmail.com>
2025-04-30 00:42:16 +0800

    about text animation

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -2,6 +2,20 @@ import React from 'react'
 import Tesseract from './models/Tesseract'
 
 export default function About() {
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+          if(entry.isIntersecting){
+              entry.target.classList.add('animated-text-show');
+          } else {
+              entry.target.classList.remove('animated-text-show');
+          }
+      });
+    });
+
+    const hiddenElements = document.querySelectorAll('.animated-text-hidden');
+    hiddenElements.forEach((el) => observer.observe(el));
+
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden flex items-center justify-center ">
 
@@ -14,18 +28,18 @@ export default function About() {
         ></video>
        
        <div className="about-text flex flex-col h-dvh mt-10 justify-center font-robert-regular text-md">
-            <p className="py-7 pl-10 font-inter">
+            <p className="py-7 pl-10 animated-text-hidden">
                 I am an aspiring web developer with a passion for crafting 
                 websites that are as beautiful as they are functional. As a self-taught developer, 
                 I’m on a journey of constant learning, diving into new technologies to bring my creative 
                 visions to life. My artistic roots drive me to prioritize design and aesthetics, blending 
                 them seamlessly with solid functionality to create digital experiences that stand out.</p>
-            <p className="py-7 pl-10">
+            <p className="py-7 pl-10 animated-text-hidden">
                 I believe that great web development is about more than just code—it’s immerse telling a story 
                 through thoughtful design and intuitive user experiences. Whether it’s a sleek layout, a 
                 responsive interface, or a perfect balance of form and function, I’m dedicated to making 
                 every project I tackle both visually captivating and technically sound.</p>
-            <p className="py-7 pl-10">
+            <p className="py-7 pl-10 animated-text-hidden">
                 When I’m not behind the screen, I’m usually out in the world that inspires me—hiking through 
                 rugged trails, camping under a starry sky, or soaking up the sun at the beach. Photography is 
                 my way of capturing those moments, while fitness keeps me energized and pushing my limits. These 
