5c43b8c5a52e56ff77781a9e73ca688700b58ccb
Joseph Lau <lau.joseph.ca@gmail.com>
2025-05-01 00:44:03 +0800

    projects

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -38,10 +38,20 @@ export default function Projects() {
                         <span className="pill">CSS</span>
                         <span className="pill">Three.js</span>
                         <span className="pill">GSAP</span>
-                        <p>A dynamic personal portfolio showcasing my journey as a self-taught web developer. Built with React and Tailwind CSS 
-                            for a sleek, responsive design, it features immersive 3D visuals powered by Three.js and smooth animations via GSAP. 
-                            My artistic passion shines through in the carefully crafted UI, blending aesthetics with seamless functionality to 
-                            create an engaging user experience.</p>
+                        <p>
+                            <div className="py-3">
+                                A dynamic personal portfolio showcasing my journey as a self-taught web developer.
+                            </div> 
+                            
+                            <div className="py-3">
+                                Built with React and Tailwind CSS 
+                                for a sleek, responsive design, it features immersive 3D visuals powered by Three.js and smooth animations via GSAP.
+                            </div>
+                            <div className="py-3">
+                                My artistic passion shines through in the carefully crafted UI, blending aesthetics with seamless functionality to 
+                                create an engaging user experience.
+                            </div>
+                        </p>
                     </div>
                 </div>
             </a>
@@ -55,11 +65,24 @@ export default function Projects() {
                         <span className="pill">React Router</span>
                         <span className="pill">Bootstrap</span>
                         <span className="pill">CSS</span>
-                        <p>A real-world web application developed for a Chinese medicine clinic — Altruism Healthcare, designed to streamline 
-                            patient interactions and showcase services. Built with React and React Router for seamless 
-                            navigation, and styled with custom CSS for a clean, professional aesthetic. 
-                            This project reflects my ability to deliver functional, user-centered solutions with a focus 
-                            on elegant design. (Desktop version in progress)</p>
+                        <p>  
+                            <div className="py-1">
+                            A real-world web application developed for a Chinese medicine clinic — Altruism Healthcare, designed to streamline 
+                            patient interactions and showcase services.
+                            </div> 
+                            
+                            <div className="py-1">
+                            Built with React and React Router for seamless 
+                            navigation, and styled with custom CSS for a clean, professional aesthetic.
+                            </div>
+                            <div className="py-1">
+                                This project reflects my ability to deliver functional, user-centered solutions with a focus 
+                                on elegant design.
+                            </div>
+                            <div className="py-1">
+                                (Desktop version in progress)
+                            </div>
+                        </p>
                     </div>
                 </div>
             </a>
@@ -71,10 +94,21 @@ export default function Projects() {
                     <div className="card-content font-robert-regular">
                         <span className="pill">JavaScript</span>
                         <span className="pill">CSS</span>
-                        <p>An early-stage personal project born from my passion for fitness, Eat Out is a mobile-first responsive web app for  
-                            fitness enthusiasts to track calories and macros while dining out. Built with vanilla JavaScript and CSS, it features a 
-                            user-friendly interface for browsing and filtering restaurant menu data. Designed completely from scratch with both aesthetics and usability in mind, 
-                            it empowers users to stay on track with their nutrition goals.</p>
+                        <p>  
+                            <div className="py-2">
+                                An early-stage personal project born from my passion for fitness, Eat Out is a mobile-first responsive web app for  
+                                fitness enthusiasts to track calories and macros while dining out.
+                            </div> 
+                            
+                            <div className="py-2">
+                                Built with vanilla JavaScript and CSS, it features a 
+                                user-friendly interface for browsing and filtering restaurant menu data.
+                            </div>
+                            <div className="py-2">
+                                Designed completely from scratch with both aesthetics and usability in mind, 
+                                it empowers users to stay on track with their nutrition goals.
+                            </div>
+                        </p>
                     </div>
                 </div>
             </a>   
