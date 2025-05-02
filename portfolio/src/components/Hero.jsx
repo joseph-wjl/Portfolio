e8050790275c668f8a335ca1ceef0def273cb6de
Joseph Lau <lau.joseph.ca@gmail.com>
2025-05-03 01:47:51 +0800

    Update Hero.jsx

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -85,9 +85,8 @@ export default function Hero() {
         {/* Overlay Content */}
         <div className="absolute left-0 top-0 z-40 size-full">
           <div className="mt-24 px-5 sm:px-10">
-            <h1 className="special-font hero-heading text-blue-100" id="demo">
-            {text}
-              {/* Hi, I am Joseph. */}
+            <h1 className="special-font hero-heading text-blue-100">
+              Hi, I am Joseph.
             </h1>
             <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
               I build sleek & stunning websites.
