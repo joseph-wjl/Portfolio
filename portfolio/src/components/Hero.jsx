14ecdbb765f3b7d949264112fc0ce00a64745529
Joseph Lau <lau.joseph.ca@gmail.com>
2025-05-03 01:57:49 +0800

    Update Hero.jsx

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -41,15 +41,16 @@ export default function Hero() {
       };
     }, []);
 
-     // Hide scrollbar while loading
-    useEffect(() => {
-      if (isLoading) {
-        document.body.style.overflow = "hidden"; // Disable scrolling
-      } else {
-        document.body.style.overflow = "auto"; // Enable scrolling
-      }
-    }, [isLoading]);
+    // // Hide scrollbar while loading
+    // useEffect(() => {
+    //   if (isLoading) {
+    //     document.body.style.overflow = "hidden"; // Disable scrolling
+    //   } else {
+    //     document.body.style.overflow = "auto"; // Enable scrolling
+    //   }
+    // }, [isLoading]);
 
+     
     useGSAP(() => {
       gsap.set("#video-frame", {
         clipPath: "polygon(40% 0, 90% 0, 65% 100%, 10% 100%)",
